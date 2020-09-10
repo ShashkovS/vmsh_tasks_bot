87ae4b95459155929391500d855030793d88bc84
Sergey Shashkov <sh57@yandex.ru>
2020-09-10 22:31:12 +0300

    Структура базы

--- db_helper.py
+++ db_helper.py
@@ -3,6 +3,7 @@ import os
 
 APP_PATH = os.path.dirname(os.path.realpath(__file__))
 sqlite_db_path = os.path.join(APP_PATH, 'db', 'prod_database.db')
+os.makedirs(os.path.dirname(sqlite_db_path), exist_ok=True)
 DB_TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 
@@ -10,45 +11,48 @@ def _create_base():
     with sqlite3.connect(sqlite_db_path) as conn:
         c = conn.cursor()
         c.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            chat_id INTEGER PRIMARY KEY UNIQUE ,
-            user_id int UNIQUE ,
-            type boolean,
-            name text,
-            surname text,
-            middlename text,
-            token text UNIQUE 
-        )
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                chat_id INTEGER UNIQUE,
+                type INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                surname TEXT NOT NULL,
+                middlename TEXT NULL,
+                token TEXT UNIQUE 
+            )
         """)
         c.execute("""
-        CREATE TABLE IF NOT EXISTS problems (
-            id INTEGER PRIMARY KEY UNIQUE ,
-            list INTEGER,
-            prob INTEGER,
-            item text,
-            UNIQUE (list, prob, item)
-        )
+            CREATE TABLE IF NOT EXISTS problems (
+                id INTEGER PRIMARY KEY,
+                list INTEGER NOT NULL,
+                prob INTEGER NOT NULL,
+                item TEXT NOT NULL,
+                UNIQUE (list, prob, item)
+            )
         """)
         c.execute("""
-        CREATE TABLE IF NOT EXISTS states (
-            user_id INTEGER PRIMARY KEY UNIQUE ,
-            state int,
-            problem_id int,
-            FOREIGN KEY(user_id) REFERENCES users(user_id)
-        )
+            CREATE TABLE IF NOT EXISTS states (
+                user_id INTEGER PRIMARY KEY,
+                state INTEGER,
+                problem_id INTEGER,
+                FOREIGN KEY(user_id) REFERENCES users(id),
+                FOREIGN KEY(problem_id) REFERENCES problems(id)
+            )
         """)
         c.execute("""
-        CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY UNIQUE ,
-            from_bot boolean,
-            tg_msg_id int,
-            chat_id int,
-            student_id int,
-            teacher_id int,
-            ts timestamp,
-            msg_text text,
-            attach_path text
-        )
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY UNIQUE,
+                from_bot boolean NOT NULL,
+                tg_msg_id INTEGER NOT NULL,
+                chat_id INTEGER NOT NULL,
+                student_id INTEGER NULL,
+                teacher_id INTEGER NULL,
+                ts timestamp NOT NULL,
+                msg_text TEXT NULL,
+                attach_path TEXT NULL,
+                FOREIGN KEY(student_id) REFERENCES users(id),
+                FOREIGN KEY(teacher_id) REFERENCES users(id)
+            )
         """)
         conn.commit()
 
