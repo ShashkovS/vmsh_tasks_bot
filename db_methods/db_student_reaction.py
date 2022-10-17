991dfd3c50309a776751a840f29acf8921912088
chvtl <myemail>
2022-10-17 21:06:51 +0300

    Реформатинг реакции ученика на отрицательный вердикт + "нормализация" енумов реакций учеников + отображение реакции самому ученику.

--- db_methods/db_student_reaction.py
+++ db_methods/db_student_reaction.py
@@ -5,10 +5,35 @@ class DB_STUDENT_REACTION():
     conn: sqlite3.Connection
 
     def write_student_reaction_on_task_bad_verdict(self, tg_msg_id: int, reaction: int) -> int:
+        """Записывает в БД в отношение student_reaction
+        рекцию ученика получающего отрицательный вердикт по письменной работе.
+        """
         cur = self.conn.cursor()
-        cur.execute("""INSERT OR REPLACE INTO student_reaction
-        SELECT problem_id, datetime(), student_id, teacher_id, chat_id, (SELECT reaction_id FROM student_reaction_enum WHERE reaction = :reaction )
+        cur.execute("""
+        INSERT OR REPLACE INTO student_reaction
+        SELECT 
+            problem_id, 
+            datetime(), 
+            student_id, 
+            teacher_id, 
+            chat_id, 
+            tg_msg_id,
+            :reaction
         FROM written_tasks_discussions WHERE tg_msg_id = :tg_msg_id;
         """, {'tg_msg_id': tg_msg_id, 'reaction': reaction})
         self.conn.commit()
         return cur.lastrowid
+
+    def student_reaction(self, reaction_n: int) -> str:
+        """Возвращает текст реакции студента (вместе с эмоджи) в зависимости от номера реакции ученика."""
+        cur = self.conn.cursor()
+        cur.execute("""SELECT emoji, reaction FROM student_reaction_enum WHERE reaction_id = :reaction_n;""",
+                    {'reaction_n': reaction_n})
+        res = cur.fetchone()
+        return ' '.join((chr(res['emoji']), res['reaction']))
+
+    def student_ractions_number(self) -> int:
+        """Возвращает число реакций ученика."""
+        cur = self.conn.cursor()
+        cur.execute("""SELECT count() FROM student_reaction_enum;""")
+        return cur.fetchone()['count()']
--- migrations/0016.add_student_reaction_relation.sql
+++ migrations/0016.add_student_reaction_relation.sql
@@ -1,11 +1,14 @@
 CREATE TABLE student_reaction_enum (
     reaction_id INT PRIMARY KEY,
-    reaction
+    emoji INT,  -- ord('<emoji>')
+    reaction TEXT UNIQUE NOT NULL
 );
 
 
 INSERT INTO student_reaction_enum VALUES
-    (1, 'ok'), (2, 'not_clear'), (3, 'discussion');
+    (0, 128076, 'Ок. Всё понял.'),
+    (1, 128533, 'Что-то всё-равно непонятно...'),
+    (2, 128587, 'Не согласен!');
 
 
 CREATE TABLE student_reaction (
@@ -14,12 +17,13 @@ CREATE TABLE student_reaction (
     student_id INT,
     teacher_id INT,
     chat_id INT,
+    tg_msg_id INT,
     reaction INT,
     FOREIGN KEY (reaction) REFERENCES student_reaction_enum(reaction_id)
 );
 
 
 CREATE VIEW student_reaction_view AS
-	SELECT problem_id, ts, student_id, teacher_id, chat_id, student_reaction_enum.reaction FROM
+	SELECT problem_id, ts, student_id, teacher_id, chat_id, tg_msg_id, student_reaction_enum.reaction FROM
 		student_reaction JOIN student_reaction_enum
 		ON (student_reaction.reaction = student_reaction_enum.reaction_id);
