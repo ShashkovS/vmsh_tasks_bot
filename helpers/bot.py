f5fe4d8886282a374bb8ff3c85ebf24ee316dd11
Sergey Shashkov <sh57@yandex.ru>
2023-05-10 14:59:29 +0300

    feat: resending messages on timeout errors

--- helpers/bot.py
+++ helpers/bot.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import aiogram
 import asyncio
+import typing
 from typing import List, Union
 from aiogram.utils.exceptions import MessageNotModified, MessageToEditNotFound, ChatNotFound
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import Message
+import aiogram.types as types
+from aiogram.types import Message, base
 from helpers.config import config, logger
 from helpers.consts import CALLBACK, STATE
 
+TIMEOUTS = [0.5, 1, 2, None]
 
 # Добавляем методов, которые игнорируют некоторые ошибки
 class BotIg(aiogram.Bot):
@@ -80,6 +83,145 @@ class BotIg(aiogram.Bot):
 
     def remove_markup_after(self, messages: Union[List[Message], Message], timeout: int):
         asyncio.create_task(self.remove_markup_after_task(messages, timeout))
+
+    async def copy_message(
+            self,
+            chat_id: typing.Union[base.Integer, base.String],
+            from_chat_id: typing.Union[base.Integer, base.String],
+            message_id: base.Integer,
+            caption: typing.Optional[base.String] = None,
+            parse_mode: typing.Optional[base.String] = None,
+            caption_entities: typing.Optional[typing.List[types.MessageEntity]] = None,
+            message_thread_id: typing.Optional[base.Integer] = None,
+            disable_notification: typing.Optional[base.Boolean] = None,
+            protect_content: typing.Optional[base.Boolean] = None,
+            reply_to_message_id: typing.Optional[base.Integer] = None,
+            allow_sending_without_reply: typing.Optional[base.Boolean] = None,
+            reply_markup: typing.Union[types.InlineKeyboardMarkup, types.ReplyKeyboardMarkup, types.ReplyKeyboardRemove, types.ForceReply, None] = None,
+    ) -> types.MessageId:
+        for t in TIMEOUTS:
+            try:
+                return await super().copy_message(chat_id, from_chat_id, message_id, caption, parse_mode, caption_entities, message_thread_id, disable_notification, protect_content, reply_to_message_id, allow_sending_without_reply, reply_markup)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in copy_message...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in copy_message in a row...")
+
+    async def delete_message(
+            self,
+            chat_id: typing.Union[base.Integer, base.String],
+            message_id: base.Integer
+    ) -> base.Boolean:
+        for t in TIMEOUTS:
+            try:
+                return await super().delete_message(chat_id, message_id)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in delete_message...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in delete_message in a row...")
+
+    async def edit_message_reply_markup(
+            self,
+            chat_id: typing.Union[base.Integer, base.String, None] = None,
+            message_id: typing.Optional[base.Integer] = None,
+            inline_message_id: typing.Optional[base.String] = None,
+            reply_markup: typing.Union[types.InlineKeyboardMarkup, None] = None
+    ) -> types.Message or base.Boolean:
+        for t in TIMEOUTS:
+            try:
+                return await super().edit_message_reply_markup(chat_id, message_id, inline_message_id, reply_markup)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in edit_message_reply_markup...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in edit_message_reply_markup in a row...")
+
+    async def edit_message_text(
+            self,
+            text: base.String,
+            chat_id: typing.Union[base.Integer, base.String, None] = None,
+            message_id: typing.Optional[base.Integer] = None,
+            inline_message_id: typing.Optional[base.String] = None,
+            parse_mode: typing.Optional[base.String] = None,
+            entities: typing.Optional[typing.List[types.MessageEntity]] = None,
+            disable_web_page_preview: typing.Optional[base.Boolean] = None,
+            reply_markup: typing.Union[types.InlineKeyboardMarkup, None] = None,
+    ) -> types.Message or base.Boolean:
+        for t in TIMEOUTS:
+            try:
+                return await super().edit_message_text(text, chat_id, message_id, inline_message_id, parse_mode, entities, disable_web_page_preview, reply_markup)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in edit_message_text...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in edit_message_text in a row...")
+
+    async def forward_message(
+            self,
+            chat_id: typing.Union[base.Integer, base.String],
+            from_chat_id: typing.Union[base.Integer, base.String],
+            message_id: base.Integer,
+            message_thread_id: typing.Optional[base.Integer] = None,
+            disable_notification: typing.Optional[base.Boolean] = None,
+            protect_content: typing.Optional[base.Boolean] = None,
+    ) -> types.Message:
+        for t in TIMEOUTS:
+            try:
+                return await super().forward_message(chat_id, from_chat_id, message_id, message_thread_id, disable_notification, protect_content)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in forward_message...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in forward_message in a row...")
+
+    async def send_message(
+            self,
+            chat_id: typing.Union[base.Integer, base.String],
+            text: base.String,
+            parse_mode: typing.Optional[base.String] = None,
+            entities: typing.Optional[typing.List[types.MessageEntity]] = None,
+            disable_web_page_preview: typing.Optional[base.Boolean] = None,
+            message_thread_id: typing.Optional[base.Integer] = None,
+            disable_notification: typing.Optional[base.Boolean] = None,
+            protect_content: typing.Optional[base.Boolean] = None,
+            reply_to_message_id: typing.Optional[base.Integer] = None,
+            allow_sending_without_reply: typing.Optional[base.Boolean] = None,
+            reply_markup: typing.Union[types.InlineKeyboardMarkup, types.ReplyKeyboardMarkup, types.ReplyKeyboardRemove, types.ForceReply, None] = None,
+    ) -> types.Message:
+        for t in TIMEOUTS:
+            try:
+                return await super().send_message(chat_id, text, parse_mode, entities, disable_web_page_preview, message_thread_id, disable_notification, protect_content, reply_to_message_id, allow_sending_without_reply, reply_markup)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in send_message...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in send_message in a row...")
+
+    async def answer_callback_query(
+            self,
+            callback_query_id: base.String,
+            text: typing.Optional[base.String] = None,
+            show_alert: typing.Optional[base.Boolean] = None,
+            url: typing.Optional[base.String] = None,
+            cache_time: typing.Optional[base.Integer] = None
+    ) -> base.Boolean:
+        for t in TIMEOUTS:
+            try:
+                return await super().answer_callback_query(callback_query_id, text, show_alert, url, cache_time)
+            except asyncio.TimeoutError as e:
+                logger.error(':( TimeoutError in answer_callback_query...')
+                if t:
+                    await asyncio.sleep(t)
+                else:
+                    raise asyncio.TimeoutError("The TimeoutError in answer_callback_query in a row...")
 
 
 # Запускаем API телеграм-бота
