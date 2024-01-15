6e3151f59641a6a354548ffbe84b632410ba8f18
Sergey Shashkov <sh57@yandex.ru>
2024-01-15 19:10:58 +0300

    feat: оценки +, +/2, -

--- handlers/student_keyboards.py
+++ handlers/student_keyboards.py
@@ -35,7 +35,6 @@ def build_problems(lesson_num: int, student: User, is_sos_question=False):
         if RESULT_MODE == FEATURES.RESULT_IMMEDIATELY:
             max_verdict = VERDICT.WRONG_ANSWER if not solved else max(solved.get(prob_id, VERDICT.WRONG_ANSWER) for prob_id in synonyms_set)
             verdict_tick = VERDICT_TO_TICK[max_verdict]
-            print(problem, solved, synonyms_set, max_verdict, verdict_tick)
             if max_verdict in VERDICTS_SOLVED:
                 tick = verdict_tick
             elif synonyms_set & being_checked:
