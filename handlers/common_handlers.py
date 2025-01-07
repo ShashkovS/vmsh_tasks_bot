af6920c27cf4f4a97201a8f13f53bdf8d6210d78
Sergey Shashkov <sh57@yandex.ru>
2025-01-07 19:10:11 +0300

    feat: surveys fix

--- handlers/common_handlers.py
+++ handlers/common_handlers.py
@@ -8,6 +8,7 @@ from helpers.config import logger, config
 import db_methods as db
 from models import User, Webtoken
 from helpers.bot import reg_callback, dispatcher, bot
+from handlers.common_keyboards import build_survey
 
 
 @reg_callback(CALLBACK.REACTION)
@@ -48,6 +49,25 @@ async def prc_reaction(query: types.CallbackQuery, student: User):
         except aiogram.utils.exceptions.InvalidQueryID:
             pass
 
+
+@reg_callback(CALLBACK.SURVEY)
+async def prc_survey(query: types.CallbackQuery, user: User):
+    """Коллбек на реакцию учителя."""
+    logger.debug(f'prc_survey - {query.data}')
+    callback, user_id, survey_id, survey_type, choice_id, selection_ids = query.data.split('_')
+    user_id = int(user_id)
+    survey_id = int(survey_id)
+    choice_id = int(choice_id)
+    if survey_type == SURVEY_TYPES.RADIO:
+        selection_ids = [choice_id]
+        db.survey.update_survey_result(user_id, survey_id, selection_ids)
+    elif survey_type == SURVEY_TYPES.CHECKBOX:
+        selection_ids = set(map(int, selection_ids.split(';'))) ^ {choice_id}
+        db.survey.update_survey_result(user_id, survey_id, selection_ids)
+    survey = db.survey.get_survey_by_id(survey_id)
+    await bot.edit_message_reply_markup_ig(chat_id=query.message.chat.id, message_id=query.message.message_id,
+                                           reply_markup=build_survey(user, survey, selection_ids))
+    await bot.answer_callback_query_ig(query.id)
 
 @dispatcher.message_handler(commands=['password'])
 async def get_my_password(message: types.Message):
