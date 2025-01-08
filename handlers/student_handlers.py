2f29c1cba5d2ac933ad808c65ca6dd8346895acd
Sergey Shashkov <sh57@yandex.ru>
2025-01-08 17:04:12 +0300

    fix: format_map question_accepted

--- handlers/student_handlers.py
+++ handlers/student_handlers.py
@@ -391,7 +391,7 @@ async def prc_wait_sos_request_state(message: types.Message, student: User):
         )
     except:
         logger.info(f'Не удалось переслать SOS-сообщение в канал {config.sos_channel}')
-    await bot.send_message(chat_id=message.chat.id, text=f"Переслал сообщение.")
+    await bot.send_message(chat_id=message.chat.id, text=msgs.question_accepted)
     if student.type == USER_TYPE.UNKNOWN:
         State.set_by_user_id(student.id, STATE.GET_USER_INFO)
     else:
