2524b03c4ef5bda10edce2c52bfed460db8eb6fa
Sergey Shashkov <sergey.shashkov@leaders.tech>
2025-10-25 16:45:08 +0300

    feat: polynom bugfix

--- helpers/calc_ans_func_values.py
+++ helpers/calc_ans_func_values.py
@@ -1,7 +1,7 @@
 import re
 from typing import List, Tuple, Union
 
-VALID_ONE_PARM_FUNC = re.compile(r'[ \d+\-*/()nkijm^]+')
+VALID_ONE_PARM_FUNC = re.compile(r'[ \d+\-*/()[a-z]^]+')
 VALID_VARS = re.compile(r'[nkijm]')
 INSERT_LOST_MUL = re.compile(r'(?<=[)n\d])(?=[(n])|(?<=[)n])(?=[(\dn])')
 BAD_EXPONENT = re.compile(r'\*\*\s*(?:\D|\d\d)')
@@ -16,22 +16,28 @@ def run_expr_on(expr: str, vals: list):
 
 def calc_first_10_values(expr: str) -> Tuple[bool, Union[str, List[float]]]:
     if len(expr) > 50:
-        return False, f'Выражение слишком длинное'
+        return False, f'Expression is too long'
+        # return False, f'Выражение слишком длинное'
     if wrong := VALID_ONE_PARM_FUNC.sub('', expr):
-        return False, f'В выражении используются некорректные символы: {wrong}'
+        return False, f'The expression contains invalid characters: {wrong}'
+        # return False, f'В выражении используются некорректные символы: {wrong}'
     expr = expr.replace('^', '**')
     if BAD_EXPONENT.search(expr):
-        return False, f'Разрешается возводить в степень от 1 до 9'
+        return False, f'Allowed exponents are from 1 to 9'
+        # return False, f'Разрешается возводить в степень от 1 до 9'
     used_vars = set(VALID_VARS.findall(expr))
     if len(used_vars) > 1:
-        return False, f'В выражении разрешается использовать только одну переменную, а здесь {", ".join(used_vars)}'
+        return False, f'Only one variable is allowed in the expression, but found: {", ".join(used_vars)}'
+        # return False, f'В выражении разрешается использовать только одну переменную, а здесь {", ".join(used_vars)}'
     expr = VALID_VARS.sub('n', expr)
     expr = INSERT_LOST_MUL.sub('*', expr)
     try:
         code = compile(expr, "<string>", "eval")
     except:
-        return False, f'Некорректная формула: что-то не так со скобками или операциями'
+        return False, f'Invalid formula: check your parentheses or operations'
+        # return False, f'Некорректная формула: что-то не так со скобками или операциями'
     if code.co_names and code.co_names != ('n',):
-        return False, f'Некорректная формула: что-то не так с используемой переменной'
+        return False, f'Invalid formula: there is an issue with the variable used'
+        # return False, f'Некорректная формула: что-то не так с используемой переменной'
     result = run_expr_on(expr, RUN_ON)
     return True, result
